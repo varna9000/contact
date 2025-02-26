fac5c690ae39eddc33f456a75e1cbabbe6065d49
pdxlocations <benlipsey@gmail.com>
2025-02-25 21:30:44 -0800

    fix region set at boot

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def main(stdscr):
                 confirmation = get_list_input("Your region is UNSET.  Set it now?", "Yes",  ["Yes", "No"])
                 if confirmation == "Yes":
                     set_region()
-                    globals.interface.close()
+                    globals.interface = None
                     globals.interface = initialize_interface(args)
             logging.info("Interface initialized")
             globals.myNodeNum = get_nodeNum()
