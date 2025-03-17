c97942d35dfd43d4ebc0488b48910ab9fab4c90e
pdxlocations <benlipsey@gmail.com>
2025-03-17 16:54:50 -0700

    attribution

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 Contact - A Console UI for Meshtastic by http://github.com/pdxlocations
 Powered by Meshtastic.org
 V 1.2.2
+
+Meshtastic® is a registered trademark of Meshtastic LLC. Meshtastic software components are released under various licenses, see GitHub for details. No warranty is provided - use at your own risk.
 '''
 
 import contextlib
