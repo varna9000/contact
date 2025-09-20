31ef968e4a350a09136005f00c9ba7a5de9e6341
varna9000 <milen@aeroisk.com>
2025-09-20 18:35:15 +0300

    Added emoji support and short names for the map

--- contact/utilities/show_map.py
+++ contact/utilities/show_map.py
@@ -1,3 +1,8 @@
+# Font is downloaded from here: https://github.com/popemkt/segoe-ui-emoji
+# No licence is provided, so I guess regular copyright is applicable, hence, I just provide a reference to the creator
+# The font is generated from Microsoft's https://github.com/microsoft/fluentui-emoji
+# which has MIT licence and which I is also ok to use in this project.
+
 import staticmaps
 import s2sphere
 import libsixel
@@ -5,11 +10,14 @@ from io import BytesIO
 import os
 import sys
 from contact.utilities.singleton import ui_state
+import contact.utilities.db_handler as db_handler
 import array
 import fcntl
 import termios
-from PIL import Image
+from PIL import Image, ImageFont
 import curses
+from pathlib import Path
+
 
 class TextLabel(staticmaps.Object):
     def __init__(self, latlng: s2sphere.LatLng, text: str) -> None:
@@ -33,11 +41,38 @@ class TextLabel(staticmaps.Object):
         w = max(self._arrow, tw + 2.0 * self._margin)
         return (int(w / 2.0), int(th + 2.0 * self._margin + self._arrow), int(w / 2), 0)
 
+    def contains_emoji(self, text: str) -> bool:
+        for ch in text:
+            if ord(ch) > 0x1F000:  # rough cutoff: emojis live in higher planes
+                return True
+            return False
+
     def render_pillow(self, renderer: staticmaps.PillowRenderer) -> None:
         x, y = renderer.transformer().ll2pixel(self.latlng())
         x = x + renderer.offset_x()
 
-        left, top, right, bottom = renderer.draw().textbbox((0, 0), self._text)
+        # Increase font size for emojis only and move a bit higher in the baloon
+        if self.contains_emoji(self._text):
+            self._font_size = 24
+            y_offset = 7
+        else:
+            y_offset = 0
+
+        # Load a font that supports emojis
+        try:
+            script_dir = Path(__file__).resolve()
+            project_root = script_dir.parents[1]
+            font_path = project_root / "seguisym.ttf"
+
+            font = ImageFont.truetype(font_path, self._font_size)
+        except Exception as e:
+            # Fallback to a default font if emoji font is missing
+            with open("error.log", "a") as f:
+               print(e, file=f)
+
+            font = ImageFont.load_default()
+
+        left, top, right, bottom = renderer.draw().textbbox((0, 0), self._text, font=font)
         th = bottom - top
         tw = right - left
         w = max(self._arrow, tw + 2 * self._margin)
@@ -55,7 +90,7 @@ class TextLabel(staticmaps.Object):
 
         renderer.draw().polygon(path, fill=(255, 255, 255, 255))
         renderer.draw().line(path, fill=(255, 0, 0, 255))
-        renderer.draw().text((x - tw / 2, y - self._arrow - h / 2 - th / 2), self._text, fill=(0, 0, 0, 255))
+        renderer.draw().text((x - tw / 2, y - self._arrow - h / 2 - th / 2 - y_offset), self._text, fill=(0, 0, 0, 255), font=font)
 
 
 def get_terminal_size():
@@ -86,7 +121,12 @@ def print_map(stdscr: curses.window) -> None:
     latlng_objects = {}
 
     for node in ui_state.map_positions:
-        node_name = node["name"]
+        # Convert hex id into decimal, as this is how it's tored in the DB
+        node_decimal_id = int(node["name"][1:], 16)
+
+        # Load short name of the node from DB
+        node_name = db_handler.get_name_from_database(node_decimal_id, type="short")
+
         lat = node["positions"][0]
         lng = node["positions"][1]
 
@@ -98,6 +138,7 @@ def print_map(stdscr: curses.window) -> None:
 
         # Add the object to the context using the dictionary
         context.add_object(TextLabel(latlng_objects[node_name], node_name))
+
 
     # Test node pin
     # node1 = staticmaps.create_latlng(43.943434, 24.090991)
@@ -116,6 +157,7 @@ def print_map(stdscr: curses.window) -> None:
     data = image.tobytes()
 
     output = libsixel.sixel_output_new(lambda data, s: s.write(data), s)
+
 
     try:
         dither = libsixel.sixel_dither_new(256)
